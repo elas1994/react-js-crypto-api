1fbb9e1b6f1adc223bb96aeded1ce7e39c4ef9c3
Sale <s_skoric@hotmail.com>
2023-08-23 20:19:15 +0200

    Version 6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Coins from "./components/Coins/Coins";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Coin from "./routes/Coin/Coin";
+import CoinItem from "./components/CoinItem/CoinItem";
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -28,9 +29,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/react-js-crypto-api/" element={<Coins coins={coins} />} />
-        <Route path="/react-js-crypto-api/" element={<Coin />}>
+        <Route path="/react-js-crypto-api/coin" element={<Coin />}>
           <Route path=":coinId" element={<Coin />} />
         </Route>
+        <Route path="/react-js-crypto-api/" element={<CoinItem />} />
       </Routes>
     </>
   );
